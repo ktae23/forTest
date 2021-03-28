edb94a3d929bf97b51e5d40a06407422537f78db
ktae23 <pktpkt8917@gmail.com>
2021-03-28 19:07:24 +0900

    FEAT : Add Classes Add MemberService, MemeberDAO, MemberVO classes

--- demo/src/main/java/com/example/demo/memberController/MemberController.java
+++ demo/src/main/java/com/example/demo/memberController/MemberController.java
@@ -1,17 +1,24 @@
-package com.example.demo.member;
+package com.example.demo.memberController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.memberService.MemberService;
+
 @Controller
 public class MemberController {
 
+
+	@Autowired
+	MemberService memberService;
 	
 	@RequestMapping(value = "/memberInsert", method = RequestMethod.GET)
 	@ResponseBody
 	public void memberInsert() {
+		memberService.memberInsert();
 		
 	}
 	
