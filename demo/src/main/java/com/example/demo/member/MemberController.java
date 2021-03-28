dcac813511b6e1e4741d357f0fecb0bc433e08f3
ktae23 <pktpkt8917@gmail.com>
2021-03-28 18:38:16 +0900

    FEAT : Add MemberController

---
+++ demo/src/main/java/com/example/demo/member/MemberController.java
@@ -0,0 +1,18 @@
+package com.example.demo.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MemberController {
+
+	
+	@RequestMapping(value = "/memberInsert", method = RequestMethod.GET)
+	@ResponseBody
+	public void memberInsert() {
+		
+	}
+	
+}
